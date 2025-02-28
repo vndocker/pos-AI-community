69b561abdb586ddd06c536da586567925ba49bf1
Ban Nguyen <zeddy502@gmail.com>
2025-03-01 01:00:57 +0700

    Update baseURL env

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
-
 export const api = axios.create({
-    baseURL: API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
